2dbc153b085e1c4009c5e7f356a5376d47a5a112
hmhtom <hmhtom@gmail.com>
2022-10-13 20:17:19 -0400

    Router to Controller
    Assign correspondent controller middleware functions to designated route

--- routers/notesRouter.js
+++ routers/notesRouter.js
@@ -3,4 +3,8 @@ const express = require("express");
 const notesRouter = express.Router();
 const { get, save, remove } = require("../controller/note");
 
+//Desinate different endpoints to its desired middleware function from Controller
+notesRouter.route("/").get(get).post(save);
+notesRouter.route("/:id").delete(remove);
+
 module.exports = notesRouter;
